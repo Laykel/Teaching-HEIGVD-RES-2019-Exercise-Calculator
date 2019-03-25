e4ce2dcd3cc5497d7be6ebcc1045189994e4dfd1
Laykel <layke.luc@gmail.com>
2019-03-25 13:59:14 +0100

    Made things not dumb

--- src/ch/heigvd/res/calculator/App.java
+++ src/ch/heigvd/res/calculator/App.java
@@ -1,5 +1,7 @@
 package ch.heigvd.res.calculator;
 
+import ch.heigvd.res.calculator.server.Server;
+
 public class App {
     public static void main(String[] args) {
         System.setProperty("java.util.logging.SimpleFormatter.format", "%5$s %n");
--- src/ch/heigvd/res/calculator/server/Server.java
+++ src/ch/heigvd/res/calculator/server/Server.java
@@ -1,7 +1,18 @@
 package ch.heigvd.res.calculator.server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Server {
-	int port;
+	final static Logger LOG = Logger.getLogger(Server.class.getName());
+
+    int port;
 
 	/**
 	 * Constructor
